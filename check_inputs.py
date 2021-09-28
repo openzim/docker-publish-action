a9fd9ef7cca6ea50ed7a6a58ad233c62c9c2b4a1
renaud gaudin <rgaudin@gmail.com>
2021-09-28 13:48:48 +0000

    Supporting schedule runs
    Schedule runs don't have access to github.event.repository.default_branch
    If this is not present, query Github API to get the default_branch instead

--- check_inputs.py
+++ check_inputs.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import sys
+import urllib.request
+
+
+def get_main_branch(repository):
+    url = "https://api.github.com/repos/{repository}".format(repository=repository)
+    with urllib.request.urlopen(url) as uh:
+        return json.load(uh).get("default_branch", None)
+
+
+def getenv(name, default=None):
+    """default branch is only available on repo-related trigger events (not schedule)"""
+    if name == "DEFAULT_BRANCH":
+        return os.getenv(name, get_main_branch(os.getenv("GITHUB_REPOSITORY")))
+    return os.getenv(name, default)
 
 
 def main():
@@ -13,11 +28,11 @@ def main():
         "DOCKERFILE",
         "PLATFORMS",
         "LATEST_ON_TAG",
-        "DEFAULT_BRANCH",
         "GITHUB_ENV",
         "GITHUB_REF",
         "GITHUB_ACTION_PATH",
         "GITHUB_REPOSITORY",
+        "DEFAULT_BRANCH",
     ]
     optional_inputs = [
         "ON_MASTER",
@@ -30,27 +45,23 @@ def main():
 
     # fail early if missing this required info
     for env in required_inputs:
-        if not os.getenv(env):
+        if not getenv(env):
             print(f"missing param `{env}`, exiting.")
             return 1
 
     # `RESTRICT_TO` env prevents this from running from forked repositories
-    if os.getenv("RESTRICT_TO") and os.getenv("GITHUB_REPOSITORY") != os.getenv(
-        "RESTRICT_TO"
-    ):
-        print(
-            "not triggered on restricted-to repo, skipping.", os.getenv("RESTRICT_TO")
-        )
+    if getenv("RESTRICT_TO") and getenv("GITHUB_REPOSITORY") != getenv("RESTRICT_TO"):
+        print("not triggered on restricted-to repo, skipping.", getenv("RESTRICT_TO"))
         return 1
 
-    with open(os.getenv("GITHUB_ENV"), "a") as fh:
+    with open(getenv("GITHUB_ENV"), "a") as fh:
         for env in required_inputs + optional_inputs:
             # don't write credentials to shared env! nor don't overwrite GH ones
             if env == "CREDENTIALS" or env.startswith("GITHUB_"):
                 continue
             fh.write(
                 "{env}={value}\n".format(
-                    env=env, value=" ".join(os.getenv(env, "").strip().split("\n"))
+                    env=env, value=" ".join(getenv(env, "").strip().split("\n"))
                 )
             )
 
